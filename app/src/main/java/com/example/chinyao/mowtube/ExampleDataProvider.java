cc766d37ea184e42bf0223d50fe44db8fe9754dd
Ching-yao Liu <leoliu1313@gmail.com>
2016-07-23 01:21:13 -0700

    rename

---
+++ app/src/main/java/com/example/chinyao/mowtube/ExampleDataProvider.java
@@ -0,0 +1,120 @@
+/*
+ *    Copyright (C) 2015 Haruki Hasegawa
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.example.chinyao.mowtube;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ExampleDataProvider extends AbstractDataProvider {
+    private ArrayList<TodoModel> mData;
+    private TodoModel mLastRemovedData;
+    private int mLastRemovedPosition = -1;
+
+    public ExampleDataProvider() {
+        /*
+        mData = new LinkedList<>();
+        final String atoz = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < atoz.length(); j++) {
+                final long id = mData.size();
+                final int viewType = 0;
+                final String text = Character.toString(atoz.charAt(j));
+                final int swipeReaction = RecyclerViewSwipeManager.REACTION_CAN_SWIPE_UP | RecyclerViewSwipeManager.REACTION_CAN_SWIPE_DOWN;
+                mData.add(new TodoModel(id, viewType, text, swipeReaction));
+            }
+        }
+        */
+    }
+
+    public void addItems(ArrayList<com.example.chinyao.mowtube.TodoModel> itemsArrayList) {
+        this.mData = itemsArrayList;
+    }
+
+    public void addItem(com.example.chinyao.mowtube.TodoModel theTodoModel) {
+        mData.add(theTodoModel);
+    }
+
+    public void editItem(int position, String content) {
+        mData.get(position).content = content;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public TodoModel getItem(int index) {
+        if (index < 0 || index >= getCount()) {
+            throw new IndexOutOfBoundsException("index = " + index);
+        }
+
+        return mData.get(index);
+    }
+
+    @Override
+    public int undoLastRemoval() {
+        if (mLastRemovedData != null) {
+            int insertedPosition;
+            if (mLastRemovedPosition >= 0 && mLastRemovedPosition < mData.size()) {
+                insertedPosition = mLastRemovedPosition;
+            } else {
+                insertedPosition = mData.size();
+            }
+
+            mData.add(insertedPosition, mLastRemovedData);
+
+            mLastRemovedData = null;
+            mLastRemovedPosition = -1;
+
+            return insertedPosition;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public void moveItem(int fromPosition, int toPosition) {
+        if (fromPosition == toPosition) {
+            return;
+        }
+
+        final TodoModel item = mData.remove(fromPosition);
+
+        mData.add(toPosition, item);
+        mLastRemovedPosition = -1;
+    }
+
+    @Override
+    public void swapItem(int fromPosition, int toPosition) {
+        if (fromPosition == toPosition) {
+            return;
+        }
+
+        Collections.swap(mData, toPosition, fromPosition);
+        mLastRemovedPosition = -1;
+    }
+
+    @Override
+    public void removeItem(int position) {
+        //noinspection UnnecessaryLocalVariable
+        final TodoModel removedItem = mData.remove(position);
+
+        mLastRemovedData = removedItem;
+        mLastRemovedPosition = position;
+    }
+}
---
+++ app/src/main/java/com/example/chinyao/mowtube/TodoModel.java
@@ -0,0 +1,84 @@
+package com.example.chinyao.mowtube;
+
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.RecyclerViewSwipeManager;
+
+/**
+ * Created by chinyao on 6/20/2016.
+ */
+public class TodoModel extends AbstractDataProvider.Data {
+
+    public Long _id; // for cupboard
+
+    static private long currentId = 0;
+    public String content;
+    public String date;
+    public String priority;
+    public long mId;
+    public int mViewType;
+    public boolean mPinned;
+
+    // we need this for SQL
+    public TodoModel() {
+        this.content = "";
+        this.date = "";
+        this.priority = "";
+        this.mId = currentId;
+        this.mViewType = RecyclerViewSwipeManager.REACTION_CAN_SWIPE_UP | RecyclerViewSwipeManager.REACTION_CAN_SWIPE_DOWN;;
+        this.mPinned = false;
+        currentId++;
+    }
+
+    public TodoModel(String content, String date, String priority) {
+        if (content != null) this.content = content;
+        if (date != null) this.date = date;
+        if (priority != null) this.priority = priority;
+        this.mId = currentId;
+        this.mViewType = RecyclerViewSwipeManager.REACTION_CAN_SWIPE_UP | RecyclerViewSwipeManager.REACTION_CAN_SWIPE_DOWN;;
+        this.mPinned = false;
+        currentId++;
+    }
+
+    public void refresh(TodoModel theTodoModel) {
+        this.content = theTodoModel.content;
+        this.date = theTodoModel.date;
+        this.priority = theTodoModel.priority;
+        this.mId = theTodoModel.mId;
+        this.mViewType = theTodoModel.mViewType;
+        this.mPinned = theTodoModel.mPinned;
+    }
+
+    @Override
+    public boolean isSectionHeader() {
+        return false;
+    }
+
+    @Override
+    public int getViewType() {
+        return mViewType;
+    }
+
+    @Override
+    public long getId() {
+        return mId;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+
+    @Override
+    public String getText() {
+        return content;
+    }
+
+    @Override
+    public boolean isPinned() {
+        return mPinned;
+    }
+
+    @Override
+    public void setPinned(boolean pinned) {
+        mPinned = pinned;
+    }
+}
