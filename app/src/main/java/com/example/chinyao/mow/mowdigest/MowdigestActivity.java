68d6a8a0dcc72fdcb11d7c968ec339e2aaec7495
Ching-yao Liu <leoliu1313@gmail.com>
2016-07-31 02:49:35 -0700

    MiguelCatalan/MaterialSearchView

--- app/src/main/java/com/example/chinyao/mow/mowdigest/MowdigestActivity.java
+++ app/src/main/java/com/example/chinyao/mow/mowdigest/MowdigestActivity.java
@@ -1,12 +1,17 @@
 package com.example.chinyao.mow.mowdigest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.chinyao.mow.R;
@@ -15,6 +20,7 @@ import com.example.chinyao.mow.mowtube.MowtubeListFragment;
 import com.example.chinyao.mow.mowtube.MowtubeViewPagerAdapter;
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,10 +40,11 @@ public class MowdigestActivity extends AppCompatActivity {
     Toolbar toolbar;
     @BindView(R.id.m_tab_layout)
     TabLayout tabLayout;
+    @BindView(R.id.m_search_view)
+    MaterialSearchView searchView;
     @BindView(R.id.m_view_pager)
     ViewPager viewPager;
 
-    private List<MowdigestNews> newsDigest;
     private MowdigestFragment newsDigestFragment;
 
     public static OkHttpClient TheOkHttpClient = null;
@@ -59,13 +66,7 @@ public class MowdigestActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
-        ActionBar ab = getSupportActionBar();
-        if (ab != null) {
-            // up right menu button
-            ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-            // up left drawer button
-            // ab.setDisplayHomeAsUpEnabled(true);
-        }
+        setupSearchView();
 
         setupViewPager();
 
@@ -93,7 +94,7 @@ public class MowdigestActivity extends AppCompatActivity {
     }
 
     private void setupViewPager() {
-        newsDigest = new ArrayList<>();
+        List<MowdigestNews> newsDigest = new ArrayList<>();
 
         // 3 tabs so set it to 2
         viewPager.setOffscreenPageLimit(2);
@@ -135,5 +136,59 @@ public class MowdigestActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mowdigest_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+
+    private void setupSearchView() {
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                searchView.setVisibility(View.VISIBLE);
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MaterialSearchView.REQUEST_VOICE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && matches.size() > 0) {
+                String searchWrd = matches.get(0);
+                if (!TextUtils.isEmpty(searchWrd)) {
+                    searchView.setQuery(searchWrd, false);
+                }
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
